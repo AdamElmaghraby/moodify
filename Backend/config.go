3fea587e1533bc84a2c9feb192175e31f19875ab
Adam <adamfingersen@gmail.com>
2025-07-02 12:56:07 -0500

    added LoadEnv in config.go to access env file

---
+++ Backend/config.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Panicln("No .env file found, relying on system environment variables.")
+	} else {
+		log.Println(".env file found successfully.")
+	}
+}
+
